c834a247b813cbb85f4612ca1212b5b1591c29b5
missnmister2026 <borosdominim05@gmail.com>
2026-02-24 16:31:24 +0200

    index + js config

---
+++ firebase-config.js
@@ -0,0 +1,12 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyAZithe6b1QCTh_-iaZGnki-EfsekH7K2w",
+  authDomain: "missnmister2026.firebaseapp.com",
+  databaseURL: "https://missnmister2026-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "missnmister2026",
+  storageBucket: "missnmister2026.firebasestorage.app",
+  messagingSenderId: "410725448180",
+  appId: "1:410725448180:web:f33f136035d0a7f147e1a1",
+  measurementId: "G-6QMK4ZHC1C"
+};
+
+export default firebaseConfig;
